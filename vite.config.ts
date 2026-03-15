b4b7cd637424a9e76eb6237fdd68ff0c86c29e80
Hugosoav <hugosoav@gmail.com>
2026-03-15 13:11:49 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/kronica-brand-studio/', // <-- ADICIONE APENAS ESTA LINHA AQUI
   server: {
     host: "::",
     port: 8080,
